e575002c2578f5c945f980263a5781dc763081a9
Riju Vijayan <rvijayan@zscaler.com>
2025-01-06 18:20:14 +0530

    chore: updated dockerfile and job yaml

--- test/tests/example1.spec.ts
+++ test/tests/example1.spec.ts
@@ -1,55 +1,55 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", { tag: "@fast" }, async ({ page }) => {
+test("has title", { tag: "@regression" }, async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test("has title 1", { tag: "@slow" }, async ({ page }) => {
+test("has title 1", { tag: "@sanity" }, async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test("has title 2", { tag: "@fast" }, async ({ page }) => {
+test("has title 2", { tag: "@regression" }, async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test("has title 3", { tag: "@slow" }, async ({ page }) => {
+test("has title 3", { tag: "@sanity" }, async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test("has title 4", { tag: "@medium" }, async ({ page }) => {
+test("has title 4", { tag: "@smoke" }, async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test("has title 5", { tag: "@fast" }, async ({ page }) => {
+test("has title 5", { tag: "@regression" }, async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test("has title 6", { tag: "@fast" }, async ({ page }) => {
+test("has title 6", { tag: "@regression" }, async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test("has title 7", { tag: "@slow" }, async ({ page }) => {
+test("has title 7", { tag: "@sanity" }, async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
